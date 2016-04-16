bbf602b22b96e857b0b8ae61f82579ecba57737b
Gufeng Chen <iamnotbluffing@gmail.com>
2016-04-16 16:05:00 +0800

    commit

--- Codeforces/Main.java
+++ Codeforces/Main.java
@@ -6,53 +6,51 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.io.PrintWriter;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.StringTokenizer;
 
 public class Main {
-	
-  public static void main(String[] args) {
-    InputStream inputStream = System.in;
-    OutputStream outputStream = System.out;
-    InputReader in = new InputReader(inputStream);
-    PrintWriter out = new PrintWriter(outputStream);
-    solve(in,out);
-    out.close();
-  }
-  
-  private static void solve(InputReader in, PrintWriter out) {
-	  
-  }
+
+	public static void main(String[] args) {
+		InputStream inputStream = System.in;
+		OutputStream outputStream = System.out;
+		InputReader in = new InputReader(inputStream);
+		PrintWriter out = new PrintWriter(outputStream);
+		solve(in,out);
+		out.close();
+	}
+
+	private static void solve(InputReader in, PrintWriter out) {
+
+	}
 
 
-  static class InputReader {
-    public BufferedReader reader;
-    public StringTokenizer tokenizer;
+	static class InputReader {
+		public BufferedReader reader;
+		public StringTokenizer tokenizer;
 
-    public InputReader(InputStream stream) {
-        reader = new BufferedReader(new InputStreamReader(stream), 32768);
-        tokenizer = null;
-    }
+		public InputReader(InputStream stream) {
+			reader = new BufferedReader(new InputStreamReader(stream), 32768);
+			tokenizer = null;
+		}
 
-    public String next() {
-        while (tokenizer == null || !tokenizer.hasMoreTokens()) {
-            try {
-                tokenizer = new StringTokenizer(reader.readLine());
-            } catch (IOException e) {
-                throw new RuntimeException(e);
-            }
-        }
-        return tokenizer.nextToken();
-    }
+		public String next() {
+			while (tokenizer == null || !tokenizer.hasMoreTokens()) {
+				try {
+					tokenizer = new StringTokenizer(reader.readLine());
+				} catch (IOException e) {
+					throw new RuntimeException(e);
+				}
+			}
+			return tokenizer.nextToken();
+		}
 
-    public int nextInt() {
-        return Integer.parseInt(next());
-    }
-    
-    public long nextLong() {
-    	return Long.parseLong(next());
-    }
+		public int nextInt() {
+			return Integer.parseInt(next());
+		}
 
-  }
+		public long nextLong() {
+			return Long.parseLong(next());
+		}
+
+	}
 }
--- ProjectEuler/EulerIsGod.java
+++ ProjectEuler/EulerIsGod.java
@@ -10,7 +10,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.StringTokenizer;
 
-public class Main {
+public class EulerIsGod {
 	
   public static void main(String[] args) {
     InputStream inputStream = System.in;
