d43a324f83a3a54d28ae0b827c6c406c076c25be
Gufeng Chen <iamnotbluffing@gmail.com>
2016-06-18 20:28:31 +0800

    update

--- ProjectEuler/EulerIsGod.java
+++ ProjectEuler/EulerIsGod.java
@@ -4,84 +4,50 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.io.OutputStream;
-import java.io.PrintWriter;
-import java.util.ArrayList;
-import java.util.List;
+import java.io.PrintStream;
 import java.util.StringTokenizer;
 
-public class EulerIsGod {
-	
-  public static void main(String[] args) {
-    InputStream inputStream = System.in;
-    OutputStream outputStream = System.out;
-    InputReader in = new InputReader(inputStream);
-    PrintWriter out = new PrintWriter(outputStream);
-    solve(in,out);
-    out.close();
-  }
-  
-  
-  //private static final int MOD = (int)1e9 + 7;
-  
-  private static boolean isPrime(int n) {
-	  for (int i = 2; i*i <=n; i++) {
-		  if(n % i == 0) {
-			  return false;
-		  }
-	  }
-	  return true;
-  }
-  
-  private static void solve(InputReader in, PrintWriter out) {
-	  int n = 100;
-	  //int K = 10;
-	  List<Integer> primes = new ArrayList<Integer>();
-	  List<Integer> pow = new ArrayList<Integer>();
-	  for (int i = 2; i <= n; i++) {
-		  if (isPrime(i)) {
-			  primes.add(i);
-			  int cnt = 0;
-			  int N = n;
-			  while(N >= i) {
-				  cnt += N / i;
-				  N /= i;
-			  }
-			  pow.add(cnt);
-		  }
-	  }
-	  
-	  
-  }
+public abstract class EulerIsGod {
 
+	private static final int MOD = (int)1e9 + 7;
+	protected final InputReader in;
+	protected final PrintStream out;
 
-  static class InputReader {
-    public BufferedReader reader;
-    public StringTokenizer tokenizer;
+	public EulerIsGod() {
+		InputStream inputStream = System.in;
+		in = new InputReader(inputStream);
+		out = System.out;
+	}
 
-    public InputReader(InputStream stream) {
-        reader = new BufferedReader(new InputStreamReader(stream), 32768);
-        tokenizer = null;
-    }
+	abstract void solve();
 
-    public String next() {
-        while (tokenizer == null || !tokenizer.hasMoreTokens()) {
-            try {
-                tokenizer = new StringTokenizer(reader.readLine());
-            } catch (IOException e) {
-                throw new RuntimeException(e);
-            }
-        }
-        return tokenizer.nextToken();
-    }
+	static class InputReader {
+		public BufferedReader reader;
+		public StringTokenizer tokenizer;
 
-    public int nextInt() {
-        return Integer.parseInt(next());
-    }
-    
-    public long nextLong() {
-    	return Long.parseLong(next());
-    }
+		public InputReader(InputStream stream) {
+			reader = new BufferedReader(new InputStreamReader(stream), 32768);
+			tokenizer = null;
+		}
 
-  }
+		public String next() {
+			while (tokenizer == null || !tokenizer.hasMoreTokens()) {
+				try {
+					tokenizer = new StringTokenizer(reader.readLine());
+				} catch (IOException e) {
+					throw new RuntimeException(e);
+				}
+			}
+			return tokenizer.nextToken();
+		}
+
+		public int nextInt() {
+			return Integer.parseInt(next());
+		}
+
+		public long nextLong() {
+			return Long.parseLong(next());
+		}
+
+	}
 }
